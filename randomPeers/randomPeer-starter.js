afc896cbc80169c3866493ed811049d823cab688
1u1ucas <lucasleveque17@gmail.com>
2024-10-30 11:56:03 +0100

    RandomPeer

--- randomPeers/randomPeer-starter.js
+++ randomPeers/randomPeer-starter.js
@@ -22,3 +22,24 @@ let users = [
 
 // Création de groupes de 2 joueurs de manière aléatoire
 // si le nombre de joueurs est impair alors il y aura un groupe de 3 joueurs
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+let shuffledUsers = shuffle(users);
+
+let groups = [];
+for (let i = 0; i < shuffledUsers.length; i += 2) {
+    if (i + 1 < shuffledUsers.length) {
+        groups.push([shuffledUsers[i], shuffledUsers[i + 1]]);
+    } else {
+        groups[groups.length - 1].push(shuffledUsers[i]);
+    }
+}
+
+console.log(groups);
