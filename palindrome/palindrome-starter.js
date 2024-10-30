acccedd3eedb653d8eaad07017294a25e84aaff7
1u1ucas <lucasleveque17@gmail.com>
2024-10-30 11:19:18 +0100

    palindrome-starter.js

--- palindrome/palindrome-starter.js
+++ palindrome/palindrome-starter.js
@@ -12,3 +12,17 @@ Paris n'est pas un palindrome
 été est un palindrome
 Ressasser est un palindrome
 */
+
+function isPalindrome(word) {
+    let lowerCaseWord = word.toLowerCase();
+    let reversedWord = lowerCaseWord.split('').reverse().join('');
+    return lowerCaseWord === reversedWord;
+}
+
+words.forEach(word => {
+    if (isPalindrome(word)) {
+        console.log(`${word} est un palindrome`);
+    } else {
+        console.log(`${word} n'est pas un palindrome`);
+    }
+});
